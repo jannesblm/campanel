a1ed1d886fbf1d1cdff1914c845170a54e36b937
Jannes Blume <1903180@uad.ac.uk>
2023-01-23 23:04:16 +0000

    stuff2

--- src/components/MyCanvas.vue
+++ src/components/MyCanvas.vue
@@ -1,23 +1,30 @@
 <script setup>
-import { ref, watch } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 const props = defineProps({
-    data: Uint8Array,
+    data: Uint8Array
 })
 
 const canvas = ref(null)
 
+onMounted(() => {
+    fill(props.data)
+})
+
 watch(() => props.data, (data) => {
-    console.log(data)
+   fill(data)
+});
+
+function fill(data) {
     const ctx = canvas.value.getContext("2d")
     var imageData = ctx.createImageData(512, 288, { colorSpace: "srgb" })
     const array = Uint8ClampedArray.from(data)
     imageData.data.set(array)
 
     ctx.putImageData(imageData, 0, 0);
-});
+}
 </script>
 
 <template>
-    <canvas ref="canvas"></canvas>
+    <canvas :ref="el => canvas = el"></canvas>
 </template>
